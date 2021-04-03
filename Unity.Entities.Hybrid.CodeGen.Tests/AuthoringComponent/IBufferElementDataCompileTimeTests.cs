ce35f99580e80c7c45877da9c03f8cdd6d8d25c6
Jonas De Maeseneer <jonas.de.maeseneer@outlook.com>
2021-04-03 14:07:30 +0200

    Upgrade to 0.17

--- Unity.Entities.Hybrid.CodeGen.Tests/AuthoringComponent/IBufferElementDataCompileTimeTests.cs
+++ Unity.Entities.Hybrid.CodeGen.Tests/AuthoringComponent/IBufferElementDataCompileTimeTests.cs
@@ -1,27 +1,27 @@
 using System;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
 using NUnit.Framework;
 using Unity.CompilationPipeline.Common.Diagnostics;
 using Unity.Entities.CodeGen;
 using Unity.Entities.CodeGen.Tests;
+using UnityEngine;
+#if ROSLYN_SOURCEGEN_ENABLED
+using Unity.Entities.CodeGen.Tests.SourceGenerationTests;
+#endif
 
 namespace Unity.Entities.Hybrid.CodeGen.Tests
 {
     [TestFixture]
     class BufferElementDataCompileTimeTests : PostProcessorTestBase
     {
-        [Test]
+#if !ROSLYN_SOURCEGEN_ENABLED
+        [Test] // Source Generation, unlike IL post-processing, can handle IBufferElementData structs that contain multiple fields
         public void WrapAroundMultipleValuesThrowsError()
         {
             AssertProducesError(
                 typeof(BufferElementDataWithMultipleWrappedValues),
                 shouldContainErrors: nameof(UserError.DC0039));
         }
-
         [Test]
         public void BufferElementWithExplicitLayoutThrowsError()
         {
@@ -46,5 +46,119 @@ namespace Unity.Entities.Hybrid.CodeGen.Tests
             Assert.AreEqual(expected: expectedDiagnosticType, actual: error?.DiagnosticType);
             Assert.IsTrue(error?.MessageData.Contains(errorIdentifiers.Single()));
         }
+#else
+        [Test] // Source Generation, unlike IL post-processing, can handle IBufferElementData with strict layout
+        public void BufferElementWithExplicitLayoutThrowsNoError()
+        {
+            var code =
+                @"
+                using System.Runtime.InteropServices;
+                using Unity.Entities;
+
+                [StructLayout(LayoutKind.Explicit, Size = 10)]
+                [GenerateAuthoringComponent]
+                public struct BufferElementWithExplicitLayout : IBufferElementData
+                {
+                    [FieldOffset(3)] public byte Value;
+                }";
+
+            var compileResult =
+                TestCompiler.Compile(code, new []
+                    {
+                        typeof(GenerateAuthoringComponentAttribute),
+                        typeof(ConvertToEntity),
+                        typeof(GameObject),
+                        typeof(MonoBehaviour)
+                    });
+
+            Assert.IsTrue(compileResult.IsSuccess);
+        }
+
+        [Test]
+        public void BufferElementWithEntityAndValueTypeThrowsNoError()
+        {
+            var code =
+                @"
+                using Unity.Entities;
+
+                public struct SomeValueType { public int Value; }
+
+                [GenerateAuthoringComponent]
+                public struct BufferElementWithEntityArray : IBufferElementData
+                {
+                    public Entity Entity;
+                    public SomeValueType ValueType;
+                }";
+
+            var compileResult =
+                TestCompiler.Compile(code, new []
+                {
+                    typeof(GenerateAuthoringComponentAttribute),
+                    typeof(ConvertToEntity),
+                    typeof(GameObject),
+                    typeof(MonoBehaviour)
+                });
+
+            Assert.IsTrue(compileResult.IsSuccess);
+        }
+
+        [Test]
+        public void BufferElementWithEntityArrayThrowsError()
+        {
+            var code =
+                @"
+                using Unity.Entities;
+
+                [GenerateAuthoringComponent]
+                public struct BufferElementWithEntityArray : IBufferElementData
+                {
+                    public Entity[] EntityArray;
+                }";
+
+            var compileResult =
+                TestCompiler.Compile(code, new []
+                    {
+                        typeof(GenerateAuthoringComponentAttribute),
+                        typeof(ConvertToEntity),
+                        typeof(GameObject),
+                        typeof(MonoBehaviour)
+                    });
+
+            Assert.IsFalse(compileResult.IsSuccess);
+            Assert.IsTrue(compileResult.CompilerMessages.Any(msg =>
+                msg.message.Contains("IBufferElementData types may only contain blittable or primitive fields.")));
+        }
+
+        [Test]
+        public void BufferElementWithReferenceTypeThrowsError()
+        {
+            var code =
+                @"
+                using Unity.Entities;
+
+                public class SomeRefType { }
+
+                [GenerateAuthoringComponent]
+                public struct BufferElementWithReferenceType : IBufferElementData
+                {
+                    public SomeRefType SomeRefType;
+                }";
+
+            var compileResult =
+                TestCompiler.Compile(code, new []
+                    {
+                        typeof(GenerateAuthoringComponentAttribute),
+                        typeof(ConvertToEntity),
+                        typeof(GameObject),
+                        typeof(MonoBehaviour)
+                    });
+
+            Assert.IsFalse(compileResult.IsSuccess);
+            Assert.IsTrue(compileResult.CompilerMessages.Any(msg =>
+                msg.message.Contains("IBufferElementData types may only contain blittable or primitive fields.")));
+        }
+
+        protected override void AssertProducesInternal(Type systemType, DiagnosticType type, string[] shouldContains, bool useFailResolver = false) { }
+#endif
     }
 }
